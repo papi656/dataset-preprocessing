ecfd0d57c33a2be49d3876d72eea3148d574018b
Abhishek Pratap Singh <abhishekprataps656@gmail.com>
2023-03-23 17:50:04 +0530

    Code to convert dataset -> BIO labeled

---
+++ preprocess.py
@@ -0,0 +1,139 @@
+import spacy 
+import argparse
+import textPreprocessing
+
+
+
+#___________Approach 3 - Comparing text rather than position________
+
+
+def convert_dataset_to_BIO_format(nlp, tokenizer, preprocessor, dataset, processed_file_path):
+    given_mentions = []
+    sentence = []
+    tokenized_sent = []
+    tokenized_token = []
+    bio_tags = []
+    with open(dataset, 'r') as fh:
+        for line in fh:            
+            #line containing title(t) and abstract(a)
+            if '\t' not in line and '|' in line:
+                parts = line.split('|')
+                if len(parts[-1]) > 0:
+                    sentence.append(parts[-1].rstrip('\n'))
+
+            # lines containing mentions
+            # given_mentions has all mentions for a t+a combine
+            if '\t' in line:
+                parts = line.split('\t')
+                tmp_str = ""
+                for i in range(3,len(parts)-2):
+                    if tmp_str == "":
+                        tmp_str += parts[i]
+                    else:
+                        tmp_str = tmp_str + ' ' + parts[i]
+                given_mentions.append(tmp_str)
+            
+            # when one t+a finished reading
+            if len(line.strip('\n')) == 0:
+                if len(sentence) == 0:
+                    continue
+                for s in sentence:
+                    doc = nlp(s)
+                    for x in doc.sents:
+                        tokenized_sent.append(str(x))
+                        # if len(x) != 0:
+                        #     tokenized_sent.append("")
+                
+
+                #tokenizing sentences
+                for sent in tokenized_sent:
+                    if len(sent.strip()) == 0:
+                        tokenized_token.append(sent)
+                        continue 
+
+                    temp = tokenizer(sent)
+
+                    for x in temp:
+                        tokenized_token.append(str(x).strip())
+                    tokenized_token.append("") # to signify end of a sentence
+
+                #Adding BIO tokens
+                i = 0
+                for mention in given_mentions:
+                    splited_mention = mention.split()
+                    m_len = 0
+                    for s_m in splited_mention:
+                        m_len += len(s_m)
+                    # print(m_len)
+                    while i < len(tokenized_token):
+                        token_to_match = ""
+                        itr = -1
+                        our_len = 0
+                        steps_count = 0
+                        while our_len < m_len:
+                            if len(tokenized_token[i]) > 0 and len(mention) > 0 and preprocessor.run(tokenized_token[i][0].strip()) != preprocessor.run(mention[0].strip()):
+                                break
+                            if i + itr >= len(tokenized_token):
+                                break
+                            steps_count += 1
+                            itr += 1
+                            if token_to_match == "":
+                                token_to_match = tokenized_token[i].strip()
+                                our_len += len(tokenized_token[i].strip())
+                            elif i+itr < len(tokenized_token):
+                                token_to_match = token_to_match + ' ' + tokenized_token[i+itr].strip()
+                                our_len += len(tokenized_token[i+itr].strip())
+
+                        if preprocessor.run(token_to_match) == preprocessor.run(mention.strip()):
+                            bio_tags.append('B')
+                            for itr in range(steps_count-1):
+                                bio_tags.append('I')
+                            i += steps_count
+                            break
+                        elif len(str(tokenized_token[i]).strip()) == 0:
+                            bio_tags.append('X')
+                            i += 1 
+                        else:
+                            bio_tags.append('O')
+                            i += 1
+
+                while i < len(tokenized_token):
+                    bio_tags.append('O')
+                    i += 1
+
+                with open(processed_file_path, 'a') as fh:
+                    for token, tag in zip(tokenized_token, bio_tags):
+                        if token.strip('\n') == "" or tag == 'X':
+                            fh.write("{}\n".format(token))
+                        else:
+                            fh.write("{} {}\n".format(token, tag))
+
+                given_mentions = []
+                sentence = []
+                tokenized_sent = []
+                tokenized_token = []
+                bio_tags = []
+            
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--dataset', type=str, required=True)
+    # parser.add_argument('--pmid_file', type=str, required=False) ____used for MedMentions_____
+    parser.add_argument('--processed_file_path', type=str, required=True)
+
+    args = parser.parse_args()
+
+    #tokenizer for tokenizing the dataset
+    nlp = spacy.load('en_core_web_trf')
+    tokenizer = nlp.tokenizer
+
+    #for preprocessing the text
+    preprocessor = textPreprocessing.TextPreprocess()
+
+    convert_dataset_to_BIO_format(nlp, tokenizer, preprocessor, args.dataset, args.processed_file_path)
+
+if __name__ == '__main__':
+    main()
+
+
--- preprocess.sh
+++ preprocess.sh
@@ -3,17 +3,17 @@ then
     mkdir resources 
 fi 
 
-if [ ! -d resources/MedMentions ];
-then 
-    mkdir resources/MedMentions 
-fi 
+# if [ ! -d resources/MedMentions ];
+# then 
+#     mkdir resources/MedMentions 
+# fi 
 
-echo "Creating dicts for MedMentions."
-python3 get_dict.py \
-    --dataset datasets/MedMentions/full/data/corpus_pubtator.txt \
-    --pmid_file datasets/MedMentions/full/data/corpus_pubtator_pmids_trng.txt \
-    --mention_dict_path resources/MedMentions/mention_dict.txt \
-    --cui_dict_path resources/MedMentions/cui_dict.txt 
+# echo "Creating dicts for MedMentions."
+# python3 get_dict.py \
+#     --dataset datasets/MedMentions/full/data/corpus_pubtator.txt \
+#     --pmid_file datasets/MedMentions/full/data/corpus_pubtator_pmids_trng.txt \
+#     --mention_dict_path resources/MedMentions/mention_dict.txt \
+#     --cui_dict_path resources/MedMentions/cui_dict.txt 
 
 
 if [ ! -d resources/NCBI_disease ];
@@ -28,3 +28,54 @@ python3 get_dict.py \
     --mention_dict_path resources/NCBI_disease/mention_dict.txt \
     --cui_dict_path resources/NCBI_disease/cui_dict.txt 
 
+echo "Converting NCBI dataset to BIO tag format:"
+
+python3 preprocess.py \
+    --dataset datasets/NCBI_disease/NCBItrainset_corpus.txt \
+    --processed_file_path resources/NCBI_disease/train.txt 
+
+echo " - Training set done"
+
+python3 preprocess.py \
+    --dataset datasets/NCBI_disease/NCBIdevelopset_corpus.txt \
+    --processed_file_path resources/NCBI_disease/devel.txt 
+
+echo " - Development set done"
+
+python3 preprocess.py \
+    --dataset datasets/NCBI_disease/NCBItestset_corpus.txt \
+    --processed_file_path resources/NCBI_disease/test.txt 
+
+echo " - Test set done"
+
+if [ ! -d resources/BC5CDR ];
+then
+    mkdir resources/BC5CDR
+fi 
+
+echo "Creating dicts for BC5CDR"
+
+python3 get_dict.py \
+    --dataset datasets/BC5CDR/CDR.Corpus.v010516/CDR_TrainingSet.PubTator.txt \
+    --mention_dict_path resources/BC5CDR/mention_dict.txt \
+    --cui_dict_path resources/BC5CDR/cui_dict.txt 
+
+echo "Converting BC5CDR dataset to BIO tag format"
+
+python3 preprocess.py \
+    --dataset datasets/BC5CDR/CDR.Corpus.v010516/CDR_TrainingSet.PubTator.txt \
+    --processed_file_path resources/BC5CDR/train.txt 
+
+echo " - Training set done"
+
+python3 preprocess.py \
+    --dataset datasets/BC5CDR/CDR.Corpus.v010516/CDR_DevelopmentSet.PubTator.txt \
+    --processed_file_path resources/BC5CDR/devel.txt 
+
+echo " - Development set done"
+
+python3 preprocess.py \
+    --dataset datasets/BC5CDR/CDR.Corpus.v010516/CDR_TestSet.PubTator.txt \
+    --processed_file_path resources/BC5CDR/test.txt 
+
+echo " - Test set done"
